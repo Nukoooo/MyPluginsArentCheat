7fa0b9ae2d46b95f8b0e2dd945febe09c2169a04
NukoOoOoOoO <nukoooo@nukooo.cc>
2023-05-23 14:22:01 +0800

    obaka :sob: :sob: :sob: :sob: :sob:

--- MyPluginsArentCheat/EntryPoint.cs
+++ MyPluginsArentCheat/EntryPoint.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Dalamud.IoC;
 using Dalamud.Logging;
 using Dalamud.Plugin;
@@ -17,19 +13,44 @@ namespace MyPluginsArentCheat;
 public class EntryPoint : IDalamudPlugin
 {
     private readonly object _pluginManager;
+    private readonly Type _pluginManagerType;
     private readonly Type _stateEnum;
-    
+
     public EntryPoint([RequiredVersion("1.0")] DalamudPluginInterface pi)
     {
-         _pluginManager = Exposed.From(pi.GetType().Assembly.
-                                            GetType("Dalamud.Service`1", true)
-                                           .MakeGenericType(pi.GetType().Assembly.GetType("Dalamud.Plugin.Internal.PluginManager", true)))
-                                   .Get();
+        _pluginManager = Exposed.From(pi.GetType()
+                                        .Assembly.GetType("Dalamud.Service`1", true)
+                                        .MakeGenericType(pi.GetType().Assembly.GetType("Dalamud.Plugin.Internal.PluginManager", true)))
+                                .Get();
         _stateEnum = pi.GetType().Assembly.GetType("Dalamud.Plugin.Internal.Types.PluginState");
-        
+
+        var service = typeof(DalamudPluginInterface).Assembly.GetTypes().First(i => i.Name.Contains("Service`1"));
+        var types = typeof(DalamudPluginInterface).Assembly.GetTypes();
+
+        _pluginManagerType = types.First(i => i.Name == "PluginManager");
+        var pluginManagerInstance = service.MakeGenericType(_pluginManagerType);
+        pluginManagerInstance.GetMethod("Get")?.Invoke(null, null);
+
+        RemoveBannedPlugins();
         UnbanInstalledPlugins();
     }
-    
+
+    public string Name => "MyPluginsArentCheat";
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+
+    private void RemoveBannedPlugins()
+    {
+        var bannedPlugins = _pluginManagerType.GetField("bannedPlugins", BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new NullReferenceException("Cannot find bannedPlugins");
+
+        var arrayType = bannedPlugins.FieldType.GetElementType();
+        var emptyArray = Array.CreateInstance(arrayType, 0);
+        bannedPlugins.SetValue(_pluginManager, emptyArray);
+    }
+
     private void UnbanInstalledPlugins()
     {
         var installedPlugins = Exposed.From(_pluginManager).InstalledPlugins;
@@ -45,18 +66,9 @@ public class EntryPoint : IDalamudPlugin
             }
 
             var banned = localPlugin.GetType().GetProperty("IsBanned", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).GetValue(localPlugin);
-            if ((bool)banned)
-            {
-                localPlugin.GetType().GetField("<IsBanned>k__BackingField", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).SetValue(localPlugin, false);
-            }
+            if ((bool)banned) localPlugin.GetType().GetField("<IsBanned>k__BackingField", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).SetValue(localPlugin, false);
             banned = localPlugin.GetType().GetProperty("IsBanned", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).GetValue(localPlugin);
+            PluginLog.Warning($"{localPlugin.GetType().GetProperty("Name", BindingFlags.Public | BindingFlags.Instance).GetValue(localPlugin)} / {banned}");
         }
-    }
-
-    public string Name => "MyPluginsArentCheat";
-
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
     }
 }
